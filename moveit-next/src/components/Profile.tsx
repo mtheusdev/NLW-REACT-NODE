8599fa1f2ad647f3fe5220d6ff274d43b92e0cff
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-04 22:54:37 -0300

    iniciando projeto react nlw

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src="https://github.com/mtheusdev.png" alt="Matheus Henrique"/>
+            <div>
+                <strong>Matheus Henrique</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import {Profile} from '../components/Profile'
+import styles from '../styles/pages/Home.module.css'
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <ExperienceBar/>
+
+      <section>
+      <div>
+        <Profile />
+      </div>
+      <div>
+        
+      </div>
+
+      </section>
+    </div>
+  )
+}
